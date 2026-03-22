9eead46ce8a76b406bb7f0c1116d5057b7a1e60f
rnguyen-rrc <rnguyen@academic.rrc.ca>
2026-03-22 09:30:07 -0500

    Allow users to search by category using dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   get "about", to: "pages#about_us"
   get "menu", to: "recipes#menu"
+  
   resources :recipes, only: [:show]
   resources :categories, only: [:index, :show]
   resources :areas, only: [:index, :show]
